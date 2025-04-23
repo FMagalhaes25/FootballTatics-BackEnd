fd08a861a9187df4dcf9c4504543366d80370c3b
Felipe Magalhães <felipemostaco05@gmail.com>
2025-04-23 20:13:31 -0300

    Inicializando o projeto Django com estrutura base configurada, incluindo Django REST Framework, documentação via Swagger e filtros.

---
+++ core/urls.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+#Configuração do Swagger
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API para criação e controle dos Elencos de Futebol",
+        default_version='v1',
+        description="Documentação da API para criação e controle dos Elencos de Futebol",
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path('api/', include('api.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+]
